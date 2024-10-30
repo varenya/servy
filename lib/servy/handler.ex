e535519a50690c22a3527c6f5f262b07c77237fd
varenya <varen90@gmail.com>
2024-10-30 20:51:09 +0530

    [VT] add a struct

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -3,69 +3,58 @@ defmodule Servy.Handler do
   import Servy.Parser, only: [parse: 1]
   import Servy.FileHandler, only: [get_page: 1, handle_file: 2]
 
-  @status_message_map %{
-    200 => "OK",
-    201 => "Created",
-    401 => "Unauthorized",
-    403 => "Forbidden",
-    404 => "Not Found",
-    500 => "Internal Server Error"
-  }
+  alias Servy.Conv
 
   def handle(request) do
     request |> parse() |> rewrite_path() |> route() |> track() |> format_response()
   end
 
-  def route(%{method: "GET", path: "/wildlife"} = conv) do
-    %{conv | resp_body: "Bears, Lions, Tigers", status: 200}
+  def route(%Conv{method: "GET", path: "/wildlife"} = conv) do
+    %Conv{conv | resp_body: "Bears, Lions, Tigers", status: 200}
   end
 
-  def route(%{method: "GET", path: "/bears"} = conv) do
-    %{conv | resp_body: "Teddy, Smokey, Paddington", status: 400}
+  def route(%Conv{method: "GET", path: "/bears"} = conv) do
+    %Conv{conv | resp_body: "Teddy, Smokey, Paddington", status: 400}
   end
 
-  def route(%{method: "GET", path: "/bears/new"} = conv) do
+  def route(%Conv{method: "GET", path: "/bears/new"} = conv) do
     get_page("form.html")
     |> File.read()
     |> handle_file(conv)
   end
 
-  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
-    %{conv | resp_body: "Bear #{id}", status: 400}
+  def route(%Conv{method: "GET", path: "/bears/" <> id} = conv) do
+    %Conv{conv | resp_body: "Bear #{id}", status: 400}
   end
 
-  def route(%{method: "DELETE", path: "/bears/" <> id} = conv) do
-    %{conv | resp_body: "Deleting a bear #{id} is forbidden", status: 403}
+  def route(%Conv{method: "DELETE", path: "/bears/" <> id} = conv) do
+    %Conv{conv | resp_body: "Deleting a bear #{id} is forbidden", status: 403}
   end
 
-  def route(%{method: "GET", path: "/pages/" <> filename} = conv) do
+  def route(%Conv{method: "GET", path: "/pages/" <> filename} = conv) do
     get_page(filename <> ".html")
     |> File.read()
     |> handle_file(conv)
   end
 
-  def route(%{method: "GET", path: "/about"} = conv) do
+  def route(%Conv{method: "GET", path: "/about"} = conv) do
     get_page("about.html")
     |> File.read()
     |> handle_file(conv)
   end
 
-  def route(%{path: path} = conv) do
-    %{conv | resp_body: "No #{path} here!", status: 404}
+  def route(%Conv{path: path} = conv) do
+    %Conv{conv | resp_body: "No #{path} here!", status: 404}
   end
 
-  def format_response(conv) do
+  def format_response(%Conv{} = conv) do
     """
-    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    HTTP/1.1 #{Conv.full_status(conv)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
-  end
-
-  defp status_reason(code) do
-    @status_message_map[code]
   end
 end
 
--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -1,6 +1,8 @@
 defmodule Servy.Parser do
+  alias Servy.Conv
+
   def parse(request) do
     [method, path, _] = request |> String.split("\n") |> List.first() |> String.split(" ")
-    %{method: method, path: path, resp_body: "", status: nil}
+    %Conv{method: method, path: path}
   end
 end
