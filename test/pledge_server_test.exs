83144a5cd3dff7939a2262f2e55df83d25f5812f
varenya <varen90@gmail.com>
2024-11-11 09:54:41 +0530

    [VT] add tests for pledge server

---
+++ test/pledge_server_test.exs
@@ -0,0 +1,25 @@
+defmodule PledgeServerTest do
+  use ExUnit.Case
+  alias Servy.PledgeServer
+
+  setup_all %{} do
+    pid = PledgeServer.start()
+    on_exit(fn -> Process.exit(pid, :normal) end)
+  end
+
+  test "pledge server should hold only 3 recent pledges" do
+    PledgeServer.create_pledge("larry", 10)
+    PledgeServer.create_pledge("moe", 20)
+    PledgeServer.create_pledge("jack", 30)
+    PledgeServer.create_pledge("jim", 30)
+
+    assert length(PledgeServer.recent_pledges()) == 3
+  end
+
+  test "total pledge should be sum of recent pledges" do
+    PledgeServer.create_pledge("moe", 20)
+    PledgeServer.create_pledge("jack", 30)
+    PledgeServer.create_pledge("jim", 30)
+    assert PledgeServer.total_pledge() == 80
+  end
+end
