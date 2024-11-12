dd18d906c05b219559074d27df2f162bdb91b698
varenya <varen90@gmail.com>
2024-11-12 09:07:28 +0530

    [VT] refactor to use the Generic Server

--- lib/servy/four_oh_four_counter.ex
+++ lib/servy/four_oh_four_counter.ex
@@ -1,7 +1,9 @@
 defmodule Servy.FourOhFourCounter do
+  alias Servy.GenericServer
+  @name :four_oh_four
   # Client
   def start(initial \\ %{}) do
-    Agent.start(fn -> initial end, name: __MODULE__)
+    GenericServer.start(__MODULE__, initial, @name)
   end
 
   defp update_url_count(url_map, url) do
@@ -10,14 +12,29 @@ defmodule Servy.FourOhFourCounter do
   end
 
   def bump_count(url_path) do
-    Agent.update(__MODULE__, &update_url_count(&1, url_path))
+    GenericServer.call(@name, {:bump_count, url_path})
   end
 
   def get_count(url_path) do
-    Agent.get(__MODULE__, fn state -> Map.get(state, url_path, 0) end)
+    GenericServer.call(@name, {:get_count, url_path})
   end
 
   def get_counts() do
-    Agent.get(__MODULE__, fn state -> state end)
+    GenericServer.call(@name, :get_counts)
+  end
+
+  def handle_call({:bump_count, url}, state) do
+    current_count = Map.get(state, url, 0)
+    new_state = state |> Map.put(url, current_count + 1)
+    {:ok, new_state}
+  end
+
+  def handle_call(:get_counts, state) do
+   {state , state}
+  end
+
+  def handle_call({:get_count, url_path}, state) do
+    response = Map.get(state, url_path, 0)
+    {response, state}
   end
 end
