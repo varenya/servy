9bf12787f58d34a3f44454fe9ef8993f62d85c52
varenya <varen90@gmail.com>
2024-11-13 11:48:16 +0530

    [VT] create supervisor for HttpServer

---
+++ lib/servy/kick_starter.ex
@@ -0,0 +1,35 @@
+defmodule Servy.KickStarter do
+  use GenServer
+
+  def start do
+    IO.puts("Starting the kickstarter....")
+    GenServer.start(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def get_server do
+    GenServer.call(__MODULE__, :get_server)
+  end
+
+  def init(:ok) do
+    Process.flag(:trap_exit, true)
+    server_pid = start_server()
+    {:ok, server_pid}
+  end
+
+  def handle_call(:get_server, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_info({:EXIT, _pid, reason}, _state) do
+    IO.puts("HTTP Server exited due to #{inspect(reason)}")
+    server_pid = start_server()
+    {:noreply, server_pid}
+  end
+
+  defp start_server do
+    IO.puts("Starting HTTP Server...")
+    server_pid = spawn_link(Servy.HttpServer, :start, [4000])
+    Process.register(server_pid, :http_server)
+    server_pid
+  end
+end
